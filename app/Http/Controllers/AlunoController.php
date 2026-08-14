72c23f7171fe6e17105dff99a7f746a2c2325e4c
PamelaStieven <stieven.pamela13@gmail.com>
2026-08-14 13:56:18 -0300

    14/08

---
+++ app/Http/Controllers/AlunoController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Aluno;
+
+class AlunoController extends Controller
+{
+    public function index()
+    {
+        $dados = Aluno::All();
+
+        return view('aluno.list')->with(['dados' => $dados]);
+    }
+    
+    function create()
+    {
+        return view('aluno.form');
+    }
+}
